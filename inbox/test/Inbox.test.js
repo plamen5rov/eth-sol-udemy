eeedde56cc3f0231f621e1bf19e1fc21b33abb5f
Plamen Petrov <biznetmen@gmail.com>
2022-05-31 10:00:53 +0300

    Sample Mocha Test

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,1 +1,31 @@
-// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const web3 = new Web3(ganache.provider());
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+    drive() {
+        return 'vroom';
+    }
+}
+
+let car;
+
+beforeEach(() => {
+    car = new Car;
+});
+
+describe('Car class test', () => {
+    it('can park', () => {
+
+        assert.equal(car.park(), 'stopped');
+    });
+    it('can drive', () => {
+
+        assert.equal(car.drive(), 'vroom');
+    });
+})
