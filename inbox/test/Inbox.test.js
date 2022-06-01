332432a192f8550e88b729b4092bae68c6fdb076
Plamen Petrov <biznetmen@gmail.com>
2022-06-01 11:12:44 +0300

    Fetching Accounts from Ganache

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,31 +1,21 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
+const { isTypedArray } = require('util/types');
 const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
-class Car {
-    park() {
-        return 'stopped';
-    }
-    drive() {
-        return 'vroom';
-    }
-}
-
-let car;
-
 beforeEach(() => {
-    car = new Car;
+    // Get a list of all accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
+    // Use one of those accounts to deploy the contract
 });
 
-describe('Car class test', () => {
-    it('can park', () => {
+describe('Inbox', () => {
+    it('deploys a contract', () => {
 
-        assert.equal(car.park(), 'stopped');
     });
-    it('can drive', () => {
-
-        assert.equal(car.drive(), 'vroom');
-    });
-})
+});
