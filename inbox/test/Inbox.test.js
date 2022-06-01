6803b391102abf419ce0dbfb2a5e22188fce120e
Plamen Petrov <biznetmen@gmail.com>
2022-06-01 11:21:05 +0300

    Code Refactoring with async/await

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,21 +1,20 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const { isTypedArray } = require('util/types');
 const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
+let accounts;
+
+beforeEach(async () => {
     // Get a list of all accounts
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-        });
+    accounts = await web3.eth.getAccounts();
+
     // Use one of those accounts to deploy the contract
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-
+        console.log(accounts);
     });
 });
