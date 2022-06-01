958591b77dc1ae840052cbfb409977aa952123c7
Plamen Petrov <biznetmen@gmail.com>
2022-06-01 11:55:45 +0300

    Deployment with Web3

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -4,17 +4,23 @@ const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
+const {interface, bytecode} = require('../compile');
+
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
 
     // Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi, there!']})
+    .send({ from: accounts[0], gas: '1000000'});
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
+        console.log(inbox);
     });
 });
