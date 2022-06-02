79f021fd89f2504512e5302ff417fb25b3105595
Plamen Petrov <biznetmen@gmail.com>
2022-06-02 16:36:53 +0300

    Deployment to Rinkeby

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -9,7 +9,14 @@ const provider = new HDWalletProvider(
 const web3 = new Web3(provider);
 
 const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
 
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!']})
+    .send({ gas: '1000000', from: accounts[0]});
+    console.log(result);
+    provider.engine.stop();
 };
 
 deploy();
