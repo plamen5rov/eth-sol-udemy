6dcc778d6d43a73880f98fb774437c29ec183ead
Plamen Petrov <biznetmen@gmail.com>
2022-06-02 16:19:39 +0300

    Wallet Provider Setup

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,15 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'mnemonic mnemonic mnemonic mnemonic mnemonic mnemonic mnemonic mnemonic mnemonic mnemonic mnemonic mnemonic',
+    'https://rinkeby.infura.io/v3/44fd46372f1c4a06b95cb1ca25c20b1e'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+
+};
+
+deploy();
