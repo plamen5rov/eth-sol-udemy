9a9b29b0524b139eee7de2c901dafe6f875e1fe9
Plamen Petrov <biznetmen@gmail.com>
2022-06-05 09:59:01 +0300

    Testing Function Modifiers

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -71,7 +71,19 @@ describe('Lottery Contract', () => {
             assert(false);
         } catch (err) {
             assert(err);
+        }
 
+    });
+
+    it('only manager can call pickWinner', async () => {
+
+        try {
+            await lottery.methods.pickWinner().send({
+                from: accounts[1]
+            });
+            assert(false);
+        } catch (err) {
+            assert(err);
         }
 
     });
