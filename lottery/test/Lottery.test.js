fdbd1dbd4dfbf09999cf458c6864cbb5edaa15de
Plamen Petrov <biznetmen@gmail.com>
2022-06-05 09:40:26 +0300

    Asserting Entering the Lottery

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -21,5 +21,19 @@ describe('Lottery Contract', () => {
 
     it('deploys a contract', () => {
         assert.ok(lottery.options.address);
-    });    
+    });
+    
+    it('allows one account to enter', async () => {
+        await lottery.methods.enter().send({ 
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+         });
+
+         const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+         });
+
+         assert.equal(accounts[0], players[0]);
+         assert.equal(1, players.length);
+    });
 });
