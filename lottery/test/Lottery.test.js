5ec48b909adb71017f148eac4d52ef246f32a6d2
Plamen Petrov <biznetmen@gmail.com>
2022-06-05 10:27:38 +0300

    End to End Test

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -87,4 +87,18 @@ describe('Lottery Contract', () => {
         }
 
     });
+
+    it('sends money to the winner and resets the players array', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2', 'ether')
+        });
+
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+        await lottery.methods.pickWinner().send({ from: accounts[0] });
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+        const difference = finalBalance - initialBalance;
+        console.log(difference);
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
+    });
 });
