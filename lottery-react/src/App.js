cd732db52c16b99da96931b0f5b4617c6a0e0229
Plamen <macbookair@Plamen-MacBook-Air.local>
2022-06-07 11:25:32 +0300

    console.log deleted

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import web3 from './web3';
 
 function App() {
-  console.log(web3.version);
-  web3.eth.getAccounts().then(console.log);
+  
   return (
     <div className="App">
       <header className="App-header">
