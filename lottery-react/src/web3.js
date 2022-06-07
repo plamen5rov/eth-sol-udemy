d5219113a666a80cf7ec27cb64a5084f2650910b
Plamen <macbookair@Plamen-MacBook-Air.local>
2022-06-07 10:36:41 +0300

    Required Update for Web3 and Metamask Permissions

--- lottery-react/src/web3.js
+++ lottery-react/src/web3.js
@@ -1,5 +1,7 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+window.ethereum.request({ method: "eth_requestAccounts" });
+ 
+const web3 = new Web3(window.ethereum);
 
 export default web3;
