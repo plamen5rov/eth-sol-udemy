9829cd838723c66ef7b213a3e76a4c03669ffd0b
Plamen <macbookair@Plamen-MacBook-Air.local>
2022-06-12 13:12:27 +0300

    creating Campaign instances

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -17,5 +17,19 @@ beforeEach(async () => {
     factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
         .deploy({ data: compiledFactory.bytecode })
         .send({ from: accounts[0], gas: '1000000' });
+
+    await factory.methods.createCampaign('100').send({
+        from: accounts[0],
+        gas: '1000000'
+    });
+
+    [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+
+    campaign = await new web3.eth.Contract(
+        JSON.parse(compiledCampaign.interface),
+        campaignAddress
+    );
+
+    
 });
 
