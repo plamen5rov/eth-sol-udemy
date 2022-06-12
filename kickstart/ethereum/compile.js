724254e63b009bbfa05486ffe20c32627f48a133
Plamen <macbookair@Plamen-MacBook-Air.local>
2022-06-12 12:15:54 +0300

    single run compilation

---
+++ kickstart/ethereum/compile.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+
+fs.removeSync(buildPath); //delete old build folder to make room for new versions
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+
+const source = fs.readFileSync(campaignPath, 'utf-8');
+
+const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath); //create new build folder
+
+for (let contract in output) {
+    fs.outputJSONSync(
+        path.resolve(buildPath, contract + '.json'),
+        output[contract]
+
+    );
+}
+
+
+
