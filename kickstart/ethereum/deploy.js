013819c0b2f97a81778c4dbb625de973eca6fce6
Plamen Petrov <biznetmen@gmail.com>
2022-06-13 13:09:15 +0300

    refactoring deployment

--- kickstart/ethereum/deploy.js
+++ kickstart/ethereum/deploy.js
@@ -1,26 +1,25 @@
-const path = require('path');
-const fs = require('fs');
-const solc = require('solc');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
 
-const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
-const source = fs.readFileSync(lotteryPath, 'utf8');
+const provider = new HDWalletProvider(
+    'REPLACE_WITH_YOUR_MNEMONIC',
+    // remember to change this to your own phrase!
+    'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
+    // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
 
-const input = {
-  language: 'Solidity',
-  sources: {
-    'Lottery.sol': {
-      content: source,
-    },
-  },
-  settings: {
-    outputSelection: {
-      '*': {
-        '*': ['*'],
-      },
-    },
-  },
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+        .deploy({ data: bytecode })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
 };
-
-module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
-  'Lottery.sol'
-].Lottery;
+deploy();
