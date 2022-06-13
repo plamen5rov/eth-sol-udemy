403ea558b106ecd3858acc7835785ad9a1fde918
Plamen Petrov <biznetmen@gmail.com>
2022-06-13 13:15:53 +0300

    modified deploy script

--- kickstart/ethereum/deploy.js
+++ kickstart/ethereum/deploy.js
@@ -16,7 +16,7 @@ const deploy = async () => {
     console.log('Attempting to deploy from account', accounts[0]);
 
     const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-        .deploy({ data: bytecode })
+        .deploy({ data: compiledFactory.bytecode })
         .send({ gas: '1000000', from: accounts[0] });
 
     console.log('Contract deployed to', result.options.address);
