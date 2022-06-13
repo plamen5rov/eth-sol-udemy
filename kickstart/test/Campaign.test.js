5845cbe309fba1928f2b42485a49382b27f48cf3
Plamen <macbookair@Plamen-MacBook-Air.local>
2022-06-13 12:32:09 +0300

    requiring minimum contributions test

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -56,5 +56,20 @@ describe('Campaigns', () => {
         assert(isContributor);
 
     });
+
+    it('requires a minimum contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({
+                value: '90',
+                from: accounts[1]
+            });
+
+            assert(false);
+
+        } catch (error) {
+            assert(error);
+        }
+
+    });
 });
 
