566a428e6e17b0199f0a860fb7aa3aba2f4d03ed
Plamen <biznetmen@gmail.com>
2022-06-17 21:22:40 +0300

    rendering Card groups

--- kickstart/.next/static/chunks/webpack.js
+++ kickstart/.next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "97bfe79da7a75448"; }
+/******/ 		__webpack_require__.h = function() { return "f468fce6adf65d33"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
 import factory from '../ethereum/factory';
 
 
@@ -11,9 +12,20 @@ class CampaignIndex extends Component {
     }
 
 
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            };
+        });
+
+        return <Card.Group items={items} />;
+    }
 
     render() {
-        return <div>{this.props.campaigns[0]}</div>;
+        return <div>{this.renderCampaigns()}</div>;
 
 
 
