e9ae975f09c53937414ab667e5793a8b98edc62a
Plamen <biznetmen@gmail.com>
2022-06-19 12:12:15 +0300

    adding form for creating new campaigns

--- kickstart/.next/static/chunks/webpack.js
+++ kickstart/.next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e2c3ab7256e58558"; }
+/******/ 		__webpack_require__.h = function() { return "454566be0940a706"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
+import { Button, Form } from 'semantic-ui-react';
 
 class CampaignNew extends Component {
     render() {
         return (
             <Layout>
-                <h1>New Campaign</h1>
+                <h3>Create new Campaign</h3>
+                <Form>
+                    <Form.Field>
+                        <label>Minimum Contribution</label>
+                        <input placeholder='Minimum Contribution' />
+                    </Form.Field>
+
+                    <Button type='submit' primary>Create!</Button>
+                </Form>
             </Layout>
 
         );
