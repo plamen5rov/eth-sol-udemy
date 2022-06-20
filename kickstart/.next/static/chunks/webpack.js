e68eae909c0a87dc1d963e2e7a70f0a659da6aab
Plamen <biznetmen@gmail.com>
2022-06-20 10:16:12 +0300

    next routes setup

--- kickstart/.next/static/chunks/webpack.js
+++ kickstart/.next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "073fade66c7ef679"; }
+/******/ 		__webpack_require__.h = function() { return "59035702d53d8bdc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
