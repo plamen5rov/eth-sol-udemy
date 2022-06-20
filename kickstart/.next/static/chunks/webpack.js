aa58c2d98de427b15139bd77594a19aecab0c226
Plamen <biznetmen@gmail.com>
2022-06-20 09:36:54 +0300

    adding spining button

--- kickstart/.next/static/chunks/webpack.js
+++ kickstart/.next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5099b7dae125157f"; }
+/******/ 		__webpack_require__.h = function() { return "073fade66c7ef679"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -7,11 +7,14 @@ import web3 from '../../ethereum/web3';
 class CampaignNew extends Component {
     state = {
         minimumContribution: '',
-        errorMessage: ''
+        errorMessage: '',
+        loading: false
     };
 
     onSubmit = async (event) => {
         event.preventDefault();
+
+        this.setState({ loading: true, errorMessage: '' });
 
         try {
             const accounts = await web3.eth.getAccounts();
@@ -25,6 +28,7 @@ class CampaignNew extends Component {
             this.setState({ errorMessage: error.message });
         }
 
+        this.setState({ loading: false });
     };
 
     render() {
@@ -44,7 +48,7 @@ class CampaignNew extends Component {
                     </Form.Field>
                     <Message error header='Oops! Something went wrong!'
                         content={this.state.errorMessage} />
-                    <Button type='submit' primary>Create!</Button>
+                    <Button loading={this.state.loading} primary>Create!</Button>
                 </Form>
             </Layout>
 
